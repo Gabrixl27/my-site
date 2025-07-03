573bc82a7d69ba61b1dcc97610a7ef15faec669c
Gabriel Barbieri <your@email.com>
2025-07-03 19:37:41 -0400

    Fix: export Select components properly

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,8 +1,18 @@
-import * as React from "react";
+import * as React from "react"
+import {
+  Select as RadixSelect,
+  SelectTrigger as RadixSelectTrigger,
+  SelectValue as RadixSelectValue,
+  SelectContent as RadixSelectContent,
+  SelectItem as RadixSelectItem,
+  SelectGroup,
+  SelectLabel
+} from "@radix-ui/react-select"
 
-export const Select = React.forwardRef<HTMLSelectElement, React.SelectHTMLAttributes<HTMLSelectElement>>(
-  ({ className, ...props }, ref) => {
-    return <select ref={ref} className={className} {...props} />;
-  }
-);
-Select.displayName = "Select";
+export const Select = RadixSelect
+export const SelectTrigger = RadixSelectTrigger
+export const SelectValue = RadixSelectValue
+export const SelectContent = RadixSelectContent
+export const SelectItem = RadixSelectItem
+export const SelectGroup = SelectGroup
+export const SelectLabel = SelectLabel
